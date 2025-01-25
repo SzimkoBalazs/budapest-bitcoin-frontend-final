9ed63865b7b526a67be807d434588bca74117c39
BalazsSzimko <ubeats1995@gmail.com>
2025-01-25 23:33:22 +0100

    Terms and conditions page data fetch

--- src/app/[locale]/terms-and-conditions/page.js
+++ src/app/[locale]/terms-and-conditions/page.js
@@ -1,13 +1,54 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 
-const page = () => {
+async function fetchTermsData(locale) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/terms-and-condition?locale=${locale}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch terms and condition section's data");
+  }
+
+  const data = await res.json();
+  return data.data || [];
+}
+
+const page = async ({ params }) => {
+  const { locale } = await params;
+  const termsSectionData = await fetchTermsData(locale);
   return (
-          <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold">Terms and Conditions</h1>
-      <p className="mt-4">
-        Here you can write the terms and conditions content based on the
-        selected language.
-      </p>
+    <div className="container mx-auto px-4 py-8 mt-[100px] rounded-md">
+      <ReactMarkdown
+        components={{
+          h1: ({ node, ...props }) => (
+            <h1
+              className="text-4xl font-extrabold text-center text-primary-500 mt-6"
+              {...props}
+            />
+          ),
+          h2: ({ node, ...props }) => (
+            <h2
+              className="text-3xl font-bold text-secondary-600 mt-4"
+              {...props}
+            />
+          ),
+          p: ({ node, ...props }) => (
+            <p className="text-neutral-300 leading-relaxed mb-4" {...props} />
+          ),
+          a: ({ node, ...props }) => (
+            <a
+              className="text-blue-500 underline hover:text-blue-700"
+              {...props}
+            />
+          ),
+          li: ({ node, ...props }) => (
+            <li className="list-disc list-inside text-white" {...props} />
+          ),
+        }}
+      >
+        {termsSectionData.Content}
+      </ReactMarkdown>
     </div>
   );
 };
