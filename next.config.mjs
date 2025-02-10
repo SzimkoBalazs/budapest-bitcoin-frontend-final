20746470ad205da5c538a5d644b5c76392a4e9c5
BalazsSzimko <ubeats1995@gmail.com>
2025-02-10 17:36:00 +0100

    config change

--- next.config.mjs
+++ next.config.mjs
@@ -3,23 +3,6 @@ const nextConfig = {
   images: {
     domains: ["localhost", "strapi.budapestbitcoin.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*.pdf",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/pdf",
-          },
-          {
-            key: "Content-Disposition",
-            value: "attachment", // or "attachment" if download needed
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
