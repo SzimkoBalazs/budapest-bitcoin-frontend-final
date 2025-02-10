b77bb55bc46a1a5673022d5c86d378f610f3d13f
BalazsSzimko <ubeats1995@gmail.com>
2025-02-10 15:42:21 +0100

    partnership pdf added to public

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     domains: ["localhost", "strapi.budapestbitcoin.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*.pdf",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/pdf",
+          },
+          {
+            key: "Content-Disposition",
+            value: "attachment", // or "attachment" if download needed
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
