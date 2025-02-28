7c0d2c97ede3ecbb16ef4124a89a5f992ba6fb82
BalazsSzimko <ubeats1995@gmail.com>
2025-02-28 14:18:54 +0100

    speaker card picture fallback size fix

--- src/components/SinglePageComponents/SpeakersSection.js
+++ src/components/SinglePageComponents/SpeakersSection.js
@@ -1,15 +1,15 @@
-import SpeakersBigTextOutline from '../SpeakersBigTextOutline';
-import SectionMainTitle from '../SectionMainTitle';
-import SpeakerCard from '../SpeakerCard';
-import MoreSpeakersTag from '../MoreSpeakersTag';
+import SpeakersBigTextOutline from "../SpeakersBigTextOutline";
+import SectionMainTitle from "../SectionMainTitle";
+import SpeakerCard from "../SpeakerCard";
+import MoreSpeakersTag from "../MoreSpeakersTag";
 
-import SecondaryCTAButton from '../SecondaryCTAButton';
-import ContentWrapper from '@/utilities/ContentWrapper';
-import GetYourPassCTAButton from '@/components/GetYourPassCTAButton';
+import SecondaryCTAButton from "../SecondaryCTAButton";
+import ContentWrapper from "@/utilities/ContentWrapper";
+import GetYourPassCTAButton from "@/components/GetYourPassCTAButton";
 
 async function fetchSpeakersSectionData(locale) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/speakers-section?locale=${locale}`,
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/speakers-section?locale=${locale}`
   );
 
   if (!res.ok) {
@@ -22,11 +22,11 @@ async function fetchSpeakersSectionData(locale) {
 
 async function fetchSpeakerCardData(locale) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/speaker-cards?locale=${locale}&populate=*&sort=order:asc`,
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/speaker-cards?locale=${locale}&populate=*&sort=order:asc`
   );
 
   if (!res.ok) {
-    throw new Error('Failed to fetch speaker card data');
+    throw new Error("Failed to fetch speaker card data");
   }
 
   const data = await res.json();
@@ -42,10 +42,10 @@ const SpeakersSection = async ({ locale }) => {
       id="speakers"
       className="mb-[80px] md:mb-[176px] relative scroll-mt-[0px]"
       style={{
-        background: 'linear-gradient(to top right, #1f1f1f 75%, #F7931A 35%)',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        background: "linear-gradient(to top right, #1f1f1f 75%, #F7931A 35%)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
       <div className="flex w-full flex-row items-between opacity-[0.075] z-0 absolute left-0 top-0 px-[40px] pt-[80px]">
@@ -65,7 +65,7 @@ const SpeakersSection = async ({ locale }) => {
             <SectionMainTitle
               text={speakerSetionData.MainTitle}
               color="bg-secondary-600"
-              underlineWidth={'97%'}
+              underlineWidth={"97%"}
             />
             <p className="text-[rgba(255,255,255,0.80)] font-exo text-[18px] sm:text-[26px] font-normal leading-[130%] tracking-[4px] self-stretch">
               {speakerSetionData.Description}
@@ -88,22 +88,26 @@ const SpeakersSection = async ({ locale }) => {
               <div
                 key={index}
                 className={`flex min-w-fit md:min-w-[200px] justify-center items-start gap-[10px] flex-[1_0_0] ${
-                  index % 2 === 1 ? 'mt-0 md:mt-[48px]' : ''
+                  index % 2 === 1 ? "mt-0 md:mt-[48px]" : ""
                 }`}
               >
                 <SpeakerCard
-                  image={`${process.env.NEXT_PUBLIC_STRAPI_URL}${speaker.Picture.formats.small.url}`}
+                  image={
+                    speaker.Picture?.formats?.small?.url
+                      ? `${process.env.NEXT_PUBLIC_STRAPI_URL}${speaker.Picture.formats.small.url}`
+                      : `${process.env.NEXT_PUBLIC_STRAPI_URL}${speaker.Picture.formats.thumbnail.url}`
+                  }
                   speakerData={speaker}
                 />
               </div>
-            ),
+            )
         )}
       </div>
       <div className="flex flex-col mt-[32px] sm:mt-10 w-full items-center">
         <GetYourPassCTAButton
-          anchorOrButton={'anchor'}
+          anchorOrButton={"anchor"}
           locale={locale}
-          href={'/speakers'}
+          href={"/speakers"}
           buttonText={speakerSetionData.MoreSpeakersButton}
         />
       </div>
