9612b2f959e1cf0d0f1eadcb2903ef6becc5783b
BalazsSzimko <ubeats1995@gmail.com>
2025-03-02 00:02:23 +0100

    disable caching in admin coupons

--- src/app/info/admin/coupons/page.js
+++ src/app/info/admin/coupons/page.js
@@ -2,6 +2,8 @@ import React from "react";
 import CouponsPage from "./CouponsPage";
 import { getAllCoupons } from "@/app/actions/coupon";
 
+export const dynamic = 'force-dynamic';
+
 const pages = async () => {
   const coupons = await getAllCoupons();
   return <CouponsPage coupons={coupons} />;
