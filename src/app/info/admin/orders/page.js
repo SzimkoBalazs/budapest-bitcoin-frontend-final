9759b7fd7da0e2307953b806251d5a997786fc7e
BalazsSzimko <ubeats1995@gmail.com>
2025-03-02 00:09:41 +0100

    disable caching on admin routes

--- src/app/info/admin/orders/page.js
+++ src/app/info/admin/orders/page.js
@@ -2,6 +2,8 @@ import React from "react";
 import OrdersPage from "./OrdersPage";
 import { getOrdersForAdmin } from "@/app/actions/orders";
 
+export const dynamic = 'force-dynamic';
+
 const page = async () => {
   const orders = await getOrdersForAdmin();
   return <OrdersPage orders={orders} />;
--- src/app/info/admin/tickets/page.js
+++ src/app/info/admin/tickets/page.js
@@ -2,6 +2,8 @@ import React from "react";
 import TicketsPage from "./TicketsPage";
 import { getTicketsForAdmin } from "@/app/actions/ticket";
 
+export const dynamic = 'force-dynamic';
+
 const page = async () => {
   const tickets = await getTicketsForAdmin();
   return <TicketsPage tickets={tickets} />;
