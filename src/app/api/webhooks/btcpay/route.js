57c3f044597e798487368a83039b9ae471209546
BalazsSzimko <ubeats1995@gmail.com>
2025-03-03 21:37:46 +0100

    error fix in invoicedata

--- src/app/api/webhooks/btcpay/route.js
+++ src/app/api/webhooks/btcpay/route.js
@@ -18,7 +18,6 @@ import { handleContactSubscription } from "@/app/actions/brevoReminderContact";
 import fs from "fs";
 import path from "path";
 
-
 function verifyBtcPaySignature(payload, signature, secret) {
   const expectedSignature = crypto
     .createHmac("sha256", secret)
@@ -36,7 +35,6 @@ export async function POST(req) {
 
   const logFilePath = path.join(process.cwd(), "webhook_logs.txt");
 
-  
   fs.appendFile(logFilePath, logData, (err) => {
     if (err) {
       logger.error("Error appending to file:", err);
@@ -49,9 +47,11 @@ export async function POST(req) {
     return NextResponse.json({ error: "Missing signature" }, { status: 400 });
   }
   const signature = headerSignature.startsWith("sha256=")
-  ? headerSignature.slice("sha256=".length)
-  : headerSignature;
-  if (!verifyBtcPaySignature(body, signature, process.env.BTCPAY_WEBHOOK_SECRET)) {
+    ? headerSignature.slice("sha256=".length)
+    : headerSignature;
+  if (
+    !verifyBtcPaySignature(body, signature, process.env.BTCPAY_WEBHOOK_SECRET)
+  ) {
     logger.error("Invalid BTCPay signature.");
     return NextResponse.json({ error: "Invalid signature" }, { status: 403 });
   }
@@ -60,7 +60,7 @@ export async function POST(req) {
   try {
     event = JSON.parse(body);
   } catch (error) {
-    logger.error(`Webhook JSON parse hiba: ${error}`); 
+    logger.error(`Webhook JSON parse hiba: ${error}`);
     return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
   }
 
@@ -74,169 +74,173 @@ export async function POST(req) {
     return NextResponse.json({ received: true }, { status: 200 });
   }
 
-  
-  if (event && (event.type === "InvoicePaymentSettled" || event.type === "InvoiceReceivedPayment")) {
-    if(event.payment && event.payment.status === "Settled"){
-    
-    const invoice = event.payment
-    
-    const orderId = event.metadata.orderId;
+  if (
+    event &&
+    (event.type === "InvoicePaymentSettled" ||
+      event.type === "InvoiceReceivedPayment")
+  ) {
+    if (event.payment && event.payment.status === "Settled") {
+      const invoice = event.payment;
 
-    if (!orderId) {
-      logger.error("Missing orderId in invoice metadata");
-      return NextResponse.json(
-        { error: "Missing orderId in invoice metadata" },
-        { status: 400 }
-      );
-    }
+      const orderId = event.metadata.orderId;
 
-    const order = await getOrder(parseInt(orderId, 10));
-    if (!order) {
-      return NextResponse.json({ error: "Order not found" }, { status: 404 });
-    }
+      if (!orderId) {
+        logger.error("Missing orderId in invoice metadata");
+        return NextResponse.json(
+          { error: "Missing orderId in invoice metadata" },
+          { status: 400 }
+        );
+      }
 
-    
-    if (order.status === "PAID") {
-      logger.info(`Order ${order.id} already paid.`);
+      const order = await getOrder(parseInt(orderId, 10));
+      if (!order) {
+        return NextResponse.json({ error: "Order not found" }, { status: 404 });
+      }
+
+      if (order.status === "PAID") {
+        logger.info(`Order ${order.id} already paid.`);
+        return NextResponse.json({ received: true }, { status: 200 });
+      }
+
+      const amountPaid = invoice.value;
+      const satoshiAmount = Math.round(parseFloat(amountPaid) * 100000000);
+
+      try {
+        await prisma.$transaction(async (tx) => {
+          await tx.payment.create({
+            data: {
+              orderId: order.id,
+              providerId: invoice.id,
+              amountInCents: satoshiAmount,
+              currency: "EUR",
+              status: PaymentStatus.SUCCESS,
+              errorMessage: null,
+            },
+          });
+          await tx.order.update({
+            where: { id: order.id },
+            data: { status: OrderStatus.PAID },
+          });
+          if (order.couponId && order.coupon) {
+            await tx.coupon.update({
+              where: { id: order.coupon.id },
+              data: { usedRedemptions: { increment: 1 } },
+            });
+          }
+        });
+      } catch (error) {
+        logger.error(
+          `Error processing BTCPay payment transaction: ${error.stack}`
+        );
+        return NextResponse.json(
+          { error: "Internal server error" },
+          { status: 500 }
+        );
+      }
+
+      const qrCodesByItem = await generateOrderQrCodes(order);
+      logger.info("QR Codes:", qrCodesByItem);
+
+      const ticketData = {
+        orderId: order.id,
+        email: order.email,
+        // Az items tömb tartalmazza az egyes jegyek adatait:
+        items: await Promise.all(
+          qrCodesByItem.map(async (item) => {
+            const ticket = await getTicket(item.ticketId);
+            const ticketName = await ticket.name;
+            return {
+              ticketId: item.ticketId,
+              ticketName,
+              quantity: item.quantity,
+              qrCodes: item.codes,
+            };
+          })
+        ),
+      };
+
+      const result = await generateNewTicketPdf(ticketData);
+      logger.info("generateTicketPdf result:", result);
+      const { voucherId, pdfPath, expiresAt } = result;
+      await createVoucher(voucherId, order.id, pdfPath, expiresAt);
+
+      let token;
+      try {
+        token = await generateDownloadToken(voucherId, expiresAt);
+        logger.info("JWT token:", token);
+      } catch (error) {
+        logger.error(`Error generating token: ${error}`);
+      }
+
+      const downloadUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/download-ticket?token=${token}`;
+
+      const defaultInvoiceData = {
+        buyerName: "",
+        email: order.email,
+        zip: "",
+        city: "",
+        address: "",
+        taxNumber: "",
+        currency: order.currency,
+        items: order.items.map((item) => {
+          const isEUR = order.currency.toUpperCase() === "EUR";
+          let grossUnitPrice;
+          if (isEUR) {
+            grossUnitPrice = item.priceAtPurchase / 100;
+          } else {
+            grossUnitPrice = item.priceAtPurchase;
+          }
+
+          if (order.coupon && order.coupon.discountValue != null) {
+            if (order.coupon.discountType === "PERCENTAGE") {
+              grossUnitPrice =
+                grossUnitPrice * (1 - order.coupon.discountValue / 100);
+            } else if (order.coupon.discountType === "FIXED") {
+              grossUnitPrice = grossUnitPrice - order.coupon.discountValue;
+              if (grossUnitPrice < 0) grossUnitPrice = 0;
+            }
+          }
+
+          if (isEUR) {
+            grossUnitPrice = parseFloat(grossUnitPrice.toFixed(2));
+          }
+          return {
+            label: "Konferencia jegy",
+            quantity: item.quantity,
+            vat: 27,
+            grossUnitPrice: grossUnitPrice,
+            unit: "pcs",
+          };
+        }),
+      };
+
+      const invoiceResult = await createInvoice(defaultInvoiceData);
+      logger.info("Invoice result:", invoiceResult);
+
+      await sendTransactionalEmail(order, downloadUrl, invoiceResult.pdf);
+
+      const orderEmail = order.email && order.email.trim();
+      if (!orderEmail) {
+        console.error("No valid email found on order.");
+      } else {
+        try {
+          await handleContactSubscription({
+            email: orderEmail,
+            subscribe: false,
+          });
+        } catch (error) {
+          console.error(
+            "Error unsubscribing contact from Brevo list:",
+            error.stack
+          );
+        }
+      }
       return NextResponse.json({ received: true }, { status: 200 });
     }
-
-    const amountPaid = invoice.value;
-    const satoshiAmount = Math.round(parseFloat(amountPaid) * 100000000);
-
-    try {
-      await prisma.$transaction(async (tx) => {
-        await tx.payment.create({
-          data: {
-            orderId: order.id,
-            providerId: invoice.id, 
-            amountInCents: satoshiAmount,
-            currency: "EUR",
-            status: PaymentStatus.SUCCESS,
-            errorMessage: null,
-          },
-        });
-        await tx.order.update({
-          where: { id: order.id },
-          data: { status: OrderStatus.PAID },
-        });
-        if (order.couponId && order.coupon) {
-          await tx.coupon.update({
-            where: { id: order.coupon.id },
-            data: { usedRedemptions: { increment: 1 } },
-          });
-        }
-      });
-    } catch (error) {
-      logger.error(
-        `Error processing BTCPay payment transaction: ${error.stack}`
-      );
-      return NextResponse.json(
-        { error: "Internal server error" },
-        { status: 500 }
-      );
-    }
-
-    const qrCodesByItem = await generateOrderQrCodes(order);
-    logger.info("QR Codes:", qrCodesByItem);
-
-    const ticketData = {
-      orderId: order.id,
-      email: order.email,
-      // Az items tömb tartalmazza az egyes jegyek adatait:
-      items: await Promise.all(
-        qrCodesByItem.map(async (item) => {
-          
-          const ticket = await getTicket(item.ticketId);
-          const ticketName = await ticket.name;
-          return {
-            ticketId: item.ticketId,
-            ticketName,
-            quantity: item.quantity,
-            qrCodes: item.codes,
-          };
-        })
-      ),
-    };
-
-    const result = await generateNewTicketPdf(ticketData);
-    logger.info("generateTicketPdf result:", result);
-    const { voucherId, pdfPath, expiresAt } = result;
-    await createVoucher(voucherId, order.id, pdfPath, expiresAt);
-
-    
-    let token;
-    try {
-      token = await generateDownloadToken(voucherId, expiresAt);
-      logger.info("JWT token:", token);
-    } catch (error) {
-      logger.error(`Error generating token: ${error}`);
-    }
-    
-    const downloadUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/download-ticket?token=${token}`;
-
-    const defaultInvoiceData = {
-      buyerName: '', 
-      email: order.email,
-      zip: '',
-      city: '',
-      address: '',
-      taxNumber: '',
-      currency: order.currency
-      items: order.items.map((item) => {
-        const isEUR = order.currency.toUpperCase() === 'EUR';
-        let grossUnitPrice;
-        if (isEUR) {
-          
-          grossUnitPrice = item.priceAtPurchase / 100;
-        } else {
-          
-          grossUnitPrice = item.priceAtPurchase;
-        }
-        
-        if (order.coupon && order.coupon.discountValue != null) {
-          if (order.coupon.discountType === 'PERCENTAGE') {
-            
-            grossUnitPrice = grossUnitPrice * (1 - order.coupon.discountValue / 100);
-          } else if (order.coupon.discountType === 'FIXED') {
-            
-            grossUnitPrice = grossUnitPrice - order.coupon.discountValue;
-            if (grossUnitPrice < 0) grossUnitPrice = 0;
-          }
-        }
-        
-        if (isEUR) {
-          grossUnitPrice = parseFloat(grossUnitPrice.toFixed(2));
-        }
-        return {
-          label: 'Konferencia jegy',
-          quantity: item.quantity,
-          vat: 27, 
-          grossUnitPrice: grossUnitPrice, 
-          unit: 'pcs',
-        };
-      }),
-    };
-
-    const invoiceResult = await createInvoice(defaultInvoiceData);
-    logger.info("Invoice result:", invoiceResult);
-
-    await sendTransactionalEmail(order, downloadUrl, invoiceResult.pdf);
-
-    const orderEmail = order.email && order.email.trim();
-    if (!orderEmail) {
-      console.error("No valid email found on order.");
-    } else {
-      try {
-        await handleContactSubscription({ email: orderEmail, subscribe: false });
-      } catch (error) {
-        console.error("Error unsubscribing contact from Brevo list:", error.stack);
-        
-      }
-    }
-    return NextResponse.json({ received: true }, { status: 200 });
-  }
-  } else if (event && (event.type === "InvoiceExpired" || event.type === "InvoiceInvalid")) {
+  } else if (
+    event &&
+    (event.type === "InvoiceExpired" || event.type === "InvoiceInvalid")
+  ) {
     const invoice = event?.payment;
     const amountPaid = invoice.value;
     const satoshiAmount = Math.round(parseFloat(amountPaid) * 100000000);
@@ -249,7 +253,6 @@ export async function POST(req) {
 
     try {
       await prisma.$transaction(async (tx) => {
-        
         await tx.payment.create({
           data: {
             orderId: order.id,
@@ -262,7 +265,7 @@ export async function POST(req) {
               : "Payment failed",
           },
         });
-       
+
         await tx.order.update({
           where: { id: order.id },
           data: { status: OrderStatus.FAILED },
